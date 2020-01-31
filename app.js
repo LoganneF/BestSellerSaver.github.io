0ad967356251cf0af7b674eb9f03b980eeb7c4da
LoganneF <loganne.featherston@gmail.com>
2020-01-30 18:04:22 -0800

    got example embedded viewer loaded onto page

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 console.log("Its working");
 
+
+google.books.load();
+
+function initialize() {
+  var viewer = new google.books.DefaultViewer(document.getElementById('viewerCanvas'));
+  viewer.load('ISBN:0738531367');
+}
+
+google.books.setOnLoadCallback(initialize);
+
 $( () => {
 
-    
+
 });
