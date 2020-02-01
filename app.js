69bcaabb29a11b6be7dfd0a1d439c01ccba2e12a
LoganneF <loganne.featherston@gmail.com>
2020-02-01 15:01:38 -0800

    changed to nyt api, got data pulling up in web console through search bar

--- app.js
+++ app.js
@@ -1,38 +1,31 @@
-console.log("Its working");
-
-const bookSearch = ()=> {
-    var search = document.getElementById('search').value;
+$( () => {
+let bookSearch = ()=> {
+    let search = document.getElementById('search').value;
     document.getElementById('results').innerHTML = "";
     console.log(search);
 
+    let url = "https://api.nytimes.com/svc/books/v3/lists/names.json";
+    url += '?' + $.param({
+        'api-key': "7MkGrlwoGBxaAQWbjSxBpKucWrQqaSiG",
+        'q': search
+    });
+
     $.ajax({
-        url: "https//www.googleapis.com/books/v1/volumes?q=" + search,
-        dataType: "json",
-        type: "GET",
-
-        success: function(data) {
-            console.log(data)
-        },
-
-      
+        url: url, 
+        method: 'GET',
+    }).done(function(result){
+        console.log(result);
+            document.getElementById("headline").innerHTML = result.response.docs["0"].headline.main;
+    }).fail(function(err) {
+        throw err;
     });
 }
 
 document.getElementById('button').addEventListener('click', bookSearch, false);
 
-google.books.load();
-
-function initialize() {
-  var viewer = new google.books.DefaultViewer(document.getElementById('viewerCanvas'));
-  viewer.load('ISBN:0738531367');
-}
-
-google.books.setOnLoadCallback(initialize);
-
-$( () => {
-
 
 });
 
 
-//google api key = AIzaSyBJYaK4RGQCnknSQWyjqRBBdD3gLEIpuAUs
+
+
