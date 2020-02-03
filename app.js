840e5417a40e381ad378eb3dade59642d9694181
LoganneF <loganne.featherston@gmail.com>
2020-02-02 18:28:30 -0800

    using top 5 best sellers. title, author, summary, rank and image url display in the DOM. need to clean up code and add functionality, css and UI modules

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ let bookSearch = ()=> {
     document.getElementById('results').innerHTML = "";
     console.log(search);
 
-    let url = "https://api.nytimes.com/svc/books/v3/reviews.json";
+    let url = "https://api.nytimes.com/svc/books/v3/lists/overview.json";
     url += '?' + $.param({
-        'title': search,
+        'published_date': search,
         'api-key': "7MkGrlwoGBxaAQWbjSxBpKucWrQqaSiG",
         
     });
@@ -14,12 +14,15 @@ let bookSearch = ()=> {
     $.ajax({
         url: url, 
         method: 'GET',
+
     }).done(function(result){
         console.log(result);
-            document.getElementById("title").innerHTML = result.results["0"].book_title;
-            document.getElementById("author").innerHTML = result.results["0"].book_author;
-            document.getElementById("summary").innerHTML = result.results["0"].summary;
-            document.getElementById("byline").innerHTML = result.results["0"].byline;
+            document.getElementById("title").innerHTML = result.results.lists["0"].books["0"].title
+            document.getElementById("author").innerHTML = result.results.lists["0"].books["0"].author
+            document.getElementById("summary").innerHTML = result.results.lists["0"].books["0"].description
+            document.getElementById("rank").innerHTML = result.results.lists["0"].books["0"].rank
+            document.getElementById("image").innerHTML = result.results.lists["0"].books["0"].book_image
+            
     }).fail(function(err) {
         throw err;
         //need message to user. its super picky about titles
