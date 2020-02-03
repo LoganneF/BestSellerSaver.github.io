b32ed62a1cdbaa16709cb0de2bc62b64db443a11
LoganneF <loganne.featherston@gmail.com>
2020-02-03 09:44:53 -0800

    api fills first and second card with the data of the first and second ranking books on list

--- app.js
+++ app.js
@@ -17,12 +17,18 @@ let bookSearch = ()=> {
 
     }).done(function(result){
         console.log(result);
-            document.getElementById("title").innerHTML = result.results.lists["0"].books["0"].title
-            document.getElementById("author").innerHTML = result.results.lists["0"].books["0"].author
-            document.getElementById("summary").innerHTML = result.results.lists["0"].books["0"].description
-            document.getElementById("rank").innerHTML = result.results.lists["0"].books["0"].rank
-            document.getElementById("image").innerHTML = result.results.lists["0"].books["0"].book_image
-            
+        // for list item 0
+            document.getElementById("title0").innerHTML = result.results.lists["0"].books["0"].title
+            document.getElementById("author0").innerHTML = result.results.lists["0"].books["0"].author
+            document.getElementById("summary0").innerHTML = result.results.lists["0"].books["0"].description
+            document.getElementById("rank0").innerHTML = result.results.lists["0"].books["0"].rank
+            document.getElementById("image0").innerHTML = result.results.lists["0"].books["0"].book_image
+        //for list item 1
+            document.getElementById("title1").innerHTML = result.results.lists["1"].books["1"].title
+            document.getElementById("author1").innerHTML = result.results.lists["1"].books["1"].author
+            document.getElementById("summary1").innerHTML = result.results.lists["1"].books["1"].description
+            document.getElementById("rank1").innerHTML = result.results.lists["1"].books["1"].rank
+            document.getElementById("image1").innerHTML = result.results.lists["1"].books["1"].book_image
     }).fail(function(err) {
         throw err;
         //need message to user. its super picky about titles
