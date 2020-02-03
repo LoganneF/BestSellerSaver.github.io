39658f1788557fbe6c90bb7f297223a4c9176a61
LoganneF <loganne.featherston@gmail.com>
2020-02-03 09:27:40 -0800

    merged drag/drop code into book scheduler code. need to style and have api data fill into the cards on page load

--- app.js
+++ app.js
@@ -35,8 +35,21 @@ document.getElementById('button').addEventListener('click', bookSearch, false);
 
 });
 
+// DRAG/DROP JS
+let id ;
 
+function allowDrop (ev) {
+    ev.preventDefault();
+}
 
+function dragStart (ev) {
+    id=ev.target.id;
+    //alert(id);
+}
+
+function drop (ev) {
+    ev.target.append(document.getElementById(id));
+}
 
 
 
