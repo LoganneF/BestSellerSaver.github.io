7557257bd01f3c28bae539d68ec44d44c602d124
LoganneF <loganne.featherston@gmail.com>
2020-02-05 14:20:09 -0800

    added landing page

--- app.js
+++ app.js
@@ -22,31 +22,31 @@ let bookSearch = ()=> {
             document.getElementById("author0").innerHTML = result.results.lists["0"].books["0"].author
             document.getElementById("summary0").innerHTML = result.results.lists["0"].books["0"].description
             document.getElementById("rank0").innerHTML = result.results.lists["0"].books["0"].rank
-            document.getElementById("image0").innerHTML = result.results.lists["0"].books["0"].book_image
+            //document.getElementById("image0").innerHTML = result.results.lists["0"].books["0"].book_image
         //for list item 1
             document.getElementById("title1").innerHTML = result.results.lists["1"].books["1"].title
             document.getElementById("author1").innerHTML = result.results.lists["1"].books["1"].author
             document.getElementById("summary1").innerHTML = result.results.lists["1"].books["1"].description
             document.getElementById("rank1").innerHTML = result.results.lists["1"].books["1"].rank
-            document.getElementById("image1").innerHTML = result.results.lists["1"].books["1"].book_image
+            //document.getElementById("image1").innerHTML = result.results.lists["1"].books["1"].book_image
         //for list item 2
             document.getElementById("title2").innerHTML = result.results.lists["2"].books["2"].title
             document.getElementById("author2").innerHTML = result.results.lists["2"].books["2"].author
             document.getElementById("summary2").innerHTML = result.results.lists["2"].books["2"].description
             document.getElementById("rank2").innerHTML = result.results.lists["2"].books["2"].rank
-            document.getElementById("image2").innerHTML = result.results.lists["2"].books["2"].book_image
+            //document.getElementById("image2").innerHTML = result.results.lists["2"].books["2"].book_image
         //for list item 3
             document.getElementById("title3").innerHTML = result.results.lists["3"].books["3"].title
             document.getElementById("author3").innerHTML = result.results.lists["3"].books["3"].author
             document.getElementById("summary3").innerHTML = result.results.lists["3"].books["3"].description
             document.getElementById("rank3").innerHTML = result.results.lists["3"].books["3"].rank
-            document.getElementById("image3").innerHTML = result.results.lists["3"].books["3"].book_image
+            //document.getElementById("image3").innerHTML = result.results.lists["3"].books["3"].book_image
         //for list item 4
             document.getElementById("title4").innerHTML = result.results.lists["4"].books["4"].title
             document.getElementById("author4").innerHTML = result.results.lists["4"].books["4"].author
             document.getElementById("summary4").innerHTML = result.results.lists["4"].books["4"].description
             document.getElementById("rank4").innerHTML = result.results.lists["4"].books["4"].rank
-            document.getElementById("image4").innerHTML = result.results.lists["4"].books["4"].book_image
+            //document.getElementById("image4").innerHTML = result.results.lists["4"].books["4"].book_image
 
     }).fail(function(err) {
         throw err;
